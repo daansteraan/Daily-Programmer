13055bbc889d92da016043761e71699a0e238524
daansteraan <102012@CPTAF-D006629.africa.corp.sasfin.net>
2015-09-16 16:39:40 +0200

    160915-1

---
+++ number232.py
@@ -0,0 +1,21 @@
+'''
+Author: Danie Strijdom 
+Date: 160915
+'''
+
+# iteratively compare all points, 
+# find y-distance and x-distance,
+# create a dictionary taht stores the result
+# return dict key that has the smallest value
+
+# store best value is a result var
+# update variable value if smaller result is found
+
+text_file = open('number232.txt')
+element_list = []
+
+for line in text_file:
+    element = line.strip('\n')
+    element_list.append(element)
+    
+print element_list[1].strip(')' + '(').split(',')
